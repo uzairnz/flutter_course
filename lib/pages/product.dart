6411ba5a7987f755dd51c700d96930c3ba4a758d
uzairnz <uzairnz@hotmail.com>
2019-03-26 20:01:36 +0500

    Passing Data Around *Using <Map> instead of <String>

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  ProductPage(this.title, this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,13 +16,14 @@ class ProductPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Image.asset('assets/food.jpg'),
-            Container(padding: EdgeInsets.all(10.0), child:Text('Details!')),
-            Container(padding: EdgeInsets.all(10.0), child: RaisedButton(
-              color:Theme.of(context).accentColor,
-              child: Text('BACK'),
-              onPressed: () => Navigator.pop(context),
-            ))
-            
+            Container(padding: EdgeInsets.all(10.0), child: Text('Details!')),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('BACK'),
+                  onPressed: () => Navigator.pop(context),
+                ))
           ],
         ));
   }
--- lib/products_manager.dart
+++ lib/products_manager.dart
@@ -3,7 +3,7 @@ import './products.dart';
 import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
-  final String startingProduct;
+  final Map startingProduct;
   ProductManager({this.startingProduct}) {
     print('[ProductManager Widget] Constructor');
   }
@@ -15,7 +15,7 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = [];
+  List<Map<String, String>> _products = [];
   @override
   void initState() {
     print('[ProductManager State] initState()');
@@ -31,7 +31,7 @@ class _ProductManagerState extends State<ProductManager> {
     super.didUpdateWidget(oldWidget);
   }
 
-  void _addProduct(String product) {
+  void _addProduct(Map<String, String> product) {
     setState(() {
       _products.add(product);
     });
