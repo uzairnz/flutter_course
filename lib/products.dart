0860965779e40d20ca6f471a4708382751b45de2
uzairnz <uzairnz@hotmail.com>
2019-03-26 18:24:14 +0500

    Basic Navigation Implimented

--- lib/products.dart
+++ lib/products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/product.dart';
 
 class Products extends StatelessWidget {
   final List<String> products;
@@ -12,23 +13,38 @@ class Products extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image.asset('assets/food.jpg'),
-          Text(products[index])
+          Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Details'),
+                onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => ProductPage(),
+                      ),
+                    ),
+              )
+            ],
+          )
         ],
       ),
     );
   }
 
-  Widget _buildProductList() {  //Made a saperate widget return type function to return this list view 
- Widget productCards;
+  Widget _buildProductList() {
+    //Made a saperate widget return type function to return this list view
+    Widget productCards;
     if (products.length > 0) {
       // Proper method to impliment if statement
       productCards = ListView.builder(
         itemBuilder: _buildProductItem, // context & index number
         itemCount: products.length, // How many products
       );
-    } 
-    else{
-      productCards =Container(); // If you dont want to render anything just return Container()
+    } else {
+      productCards =
+          Container(); // If you dont want to render anything just return Container()
     }
     return productCards;
   }
