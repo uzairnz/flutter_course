83132cab4ffded1b5abcc29e621222340e2286f9
uzairnz <uzairnz@hotmail.com>
2019-03-26 16:54:40 +0500

    Alternative approaches to render content conditionally *Build Working

--- lib/products.dart
+++ lib/products.dart
@@ -3,27 +3,40 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> products;
 
-  Products([this.products = const []]){
+  Products([this.products = const []]) {
     print('[Product Widget] Constructor');
   }
 
-  Widget _buildProductItem(BuildContext context, int  index){
+  Widget _buildProductItem(BuildContext context, int index) {
     return Card(
-                    child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text(products[index])
-                  ],
-                    ),
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index])
+        ],
+      ),
     );
+  }
+
+  Widget _buildProductList() {  //Made a saperate widget return type function to return this list view 
+ Widget productCard;
+    if (products.length > 0) {
+      // Proper method to impliment if statement
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem, // context & index number
+        itemCount: products.length, // How many products
+      );
+    } else {
+      productCard = Center(
+        child: Text('No product found, please add some'),
+      );
+    }
+    return productCard;
   }
 
   @override
   Widget build(BuildContext context) {
     print('[Product Widget] Built');
-    return products.length > 0 ?  ListView.builder(
-      itemBuilder: _buildProductItem,  // context & index number 
-      itemCount: products.length,  // How many products
-    ) :Center(child: Text('No product found, please add some'),);   // Implimenting condition using '?' as if and ':' as else statement from 24 - 27
+    return _buildProductList();
   }
 }
